1df47364428861d7ef0c0b4168ecb318ef6bf65e
Patrick Shushereba <pshushereba@gmail.com>
2019-11-09 09:58:05 -0800

    Finished CRUD for /projects

--- routers/project-router.js
+++ routers/project-router.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const db = require('../data/helpers/projectModel.js')
+const db = require('../data/helpers/projectModel.js');
+const actionDb = require('../data/helpers/actionModel.js');
 
 // Create a router
 const router = express.Router();
@@ -12,7 +13,63 @@ router.get('/', (req, res) => {
         .catch(() => {
             res.status(500).json({message: "There was a problem retrieving the projects."})
         })
-    // res.status(200).json({ message: "It's Working! It's Working!"})
 })
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+
+    db.get(id)
+        .then((project) => {
+            res.status(200).json(project);
+        })
+        .catch(() => {
+            res.status(500).json({message: "There was a problem retrieving the project."})
+        })
+})
+
+// router.get('/:id/actions', (req, res) => {
+
+// })
+
+router.post('/', (req, res) => {
+    const projectInfo = req.body;
+    
+    db.insert(projectInfo)
+        .then((project) => {
+            res.status(201).json(project);
+        })
+        .catch(() => {
+            res.status(500).json({ message: "Error creating new project" });
+        });
+});
+
+router.put('/:id', (req, res) => {
+    const updatedProject = req.body;
+    db.update(req.params.id, updatedProject)
+      .then(() => {
+        db.get(req.params.id)
+          .then(project => {
+            res.status(201).json(project);
+          })
+          .catch(() => {
+            res
+              .status(500)
+              .json({ message: "Error retrieving updated project" });
+          });
+      })
+      .catch(() => {
+        res.status(500).json({ message: "Error updating project" });
+      });
+});
+
+router.delete('/:id', (req, res) => {
+    db.remove(req.params.id)
+        .then(() => {
+            res.status(204).end();
+        })
+        .catch(() => {
+            res.status(500).json({ message: "Error deleting project" });
+        });
+});
 
 module.exports = router;
